6cade9d2f2e05b715e19d57fd771e311f3de1eaa
Tricrepe <644568681@qq.com>
2024-10-13 14:27:11 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 宋亚唯 on 2024/10/13.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
